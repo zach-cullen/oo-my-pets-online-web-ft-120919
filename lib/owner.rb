dd9d08d7b49bfbb9cb082e2d9114d7e9acdbca3e
Zach Cullen <zacullen@gmail.com>
2020-01-07 16:29:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species)
+    @name = name
+    @species = "human"
+  end
+  
+  def say_species
+    puts @species
+  end
+  
+  # class methods
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    self.all.count()
+  end
 end
